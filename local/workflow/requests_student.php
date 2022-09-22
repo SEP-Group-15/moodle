17c9f44e7fc63d574199f21b4fe290c9cc28e860
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2022-09-22 23:02:05 +0530

    Create requests UI

---
+++ local/workflow/requests_student.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(__DIR__ . '/../../config.php'); // setup moodle
+require_login();
+$context = context_system::instance();
+
+global $DB;
+$PAGE->set_url(new moodle_url('/local/workflow/requests_student.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('Student Requests');
+$PAGE->set_heading('Assignment 1 - Student Requests');
+
+$templatecontext = (object)[
+    'text' => 'text'
+];
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->render_from_template('local_workflow/requests_student', $templatecontext);
+
+echo $OUTPUT->footer();
