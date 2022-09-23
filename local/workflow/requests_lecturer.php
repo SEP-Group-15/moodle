4ee383bb1bd2ab0214bb5c801fd01fb175605341
udith355 <udith.19@cse.mrt.ac.lk>
2022-09-23 09:32:11 +0530

    ui added

--- local/workflow/requests_lecturer.php
+++ local/workflow/requests_lecturer.php
@@ -29,17 +29,15 @@ require_login();
 $context = context_system::instance();
 
 global $DB;
-$PAGE->set_url(new moodle_url('/local/workflow/requests_instructor.php'));
+$PAGE->set_url(new moodle_url('/local/workflow/requests_lecturer.php'));
 $PAGE->set_context(\context_system::instance());
 $PAGE->set_title('Student Requests');
 $PAGE->set_heading('Assignment 1 - Student Requests');
 
-$templatecontext = (object)[
-    'text' => 'text'
-];
+$templatecontext = (object)[];
 
 echo $OUTPUT->header();
 
-echo $OUTPUT->render_from_template('local_workflow/requests_instructor', $templatecontext);
+echo $OUTPUT->render_from_template('local_workflow/requests_lecturer', $templatecontext);
 
 echo $OUTPUT->footer();
