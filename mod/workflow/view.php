b59131b00f0efa74176590388885054f8e5d17f7
BanulaMahen <banulakumarage@gmail.com>
2022-10-02 23:31:45 +0530

    Update view.php

--- mod/workflow/view.php
+++ mod/workflow/view.php
@@ -38,31 +38,19 @@ require_login();
 $instance = $DB->get_record('workflow', ['id' => $cm->instance], '*', MUST_EXIST);
 $context = context_course::instance($course->id);
 
-// echo '<pre>';
-// var_dump($cm->id);
-// echo '</pre>';
-// die();
-
 global $DB;
 
-// $sql = 'select shortname from role where id = (select roleid from mdl_role_assignments where contextid = :contextid and userid = :userid)';
-// $params = [
-// 'contextid' => $context->id,
-// 'userid' => $USER->id,
-// ];
 
 $roleid = $DB->get_field_select('role_assignments', 'roleid', 'contextid = :contextid and userid=:userid', [
     'contextid' => $context->id,
     'userid' => $USER->id,
 ]);
 
-// try {
-//     $role =  $DB->execute($sql, $params);
-// } catch (dml_exception $e) {
-//     $role = false;
-// }
+$role = $DB->get_field_select('role','shortname','id=:id',[
+    'id'=>$roleid
+]);
 
-var_dump($roleid);
+var_dump($role);
 die();
 
 // $PAGE->set_url(new moodle_url('/workflow/requests.php'));
